aeb76bcf4e7e403ace84fd66565c0bd5009ec101
netsus <cwjcsk@naver.com>
2021-12-01 23:59:41 +0900

    change src dir to reset

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyByVXwhIijKJW3bD-z0L3Q20iIn6VnxqO8",
+  authDomain: "nwitter-8e536.firebaseapp.com",
+  projectId: "nwitter-8e536",
+  storageBucket: "nwitter-8e536.appspot.com",
+  messagingSenderId: "107219779961",
+  appId: "1:107219779961:web:bf54d58449c7899b196fa8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
